1c2b98b2f8d4335581f92ef2f9837da34ca522fc
OikonomouEvangelos <oikonomouevaggelos35@gmail.com>
2025-11-27 13:12:27 +0200

    init

---
+++ src/main/java/com/CineEuxoulides/Euxoulides/EuxoulidesApplication.java
@@ -0,0 +1,13 @@
+package com.CineEuxoulides.Euxoulides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuxoulidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuxoulidesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CineEuxoulides/Euxoulides/EuxoulidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CineEuxoulides.Euxoulides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuxoulidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
