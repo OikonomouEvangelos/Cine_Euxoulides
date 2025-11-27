8e5b706e25b17cec8dd30348d24bed8a88122070
christina fotiadou <christinaphotiadou4@gmail.com>
2025-11-27 23:55:34 +0200

    init-side-menu

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,20 +1,31 @@
 // src/App.jsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
-import './App.css'; // <--- ΝΕΑ ΠΡΟΣΘΗΚΗ: Κάνουμε import το App.css
+import SideMenu from './components/SideMenu';
 
 const App = () => {
+
+      const [isMenuOpen, setIsMenuOpen] = useState(false);
+      const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+      };
+
+
+      const closeMenu = () => {
+        setIsMenuOpen(false);
+      };
   return (
     // Προσθέτουμε την κλάση app-container στο κύριο div
     <div className="app-container">
-      <Header />
 
-      {/* Η περιοχή main χρησιμοποιεί το flex-grow: 1 από το App.css */}
+      <Header onMenuToggle={toggleMenu} />
+            <SideMenu isOpen={isMenuOpen} onClose={closeMenu} />
+
       <main>
         <h1>Καλώς ήρθατε στο MyFlix!</h1>
         <p>Εδώ θα εμφανίζονται οι λίστες με τις ταινίες και τις σειρές. (Το κείμενο είναι τώρα ανοιχτό γκρι.)</p>
-        {/* Εδώ θα μπουν τα components για τις λίστες (π.χ. MovieList) */}
+
       </main>
 
     </div>
